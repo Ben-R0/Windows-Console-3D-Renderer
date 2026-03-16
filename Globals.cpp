db59c1d03de541183f2d874d4d1cea7a57a1f1e9
Ben R <benjangilo4@gmail.com>
2026-03-16 15:26:37 -0400

    Globals.cpp modified for default window size (240x80 used for fullscreen)

--- Globals.cpp
+++ Globals.cpp
@@ -3,8 +3,8 @@
 
 Camera gCamera = { Vector3(0,0,-5.0), Vector3(0.0,0.0,0.0), 90.0, 0.0, 1000.0 };
 
-const double SCREEN_WIDTH = 240.0;//120.0;
-const double SCREEN_HEIGHT = 80.0;//40.0;
+const double SCREEN_WIDTH = 120.0;//120.0;
+const double SCREEN_HEIGHT = 40.0;//40.0;
 const int nSCREEN_WIDTH = static_cast<int>(SCREEN_WIDTH);
 const int nSCREEN_HEIGHT = static_cast<int>(SCREEN_HEIGHT);
 
